8255aa8104d14197cfa68057e72943221cac65f4
HubBrej <43384197+HubBrej@users.noreply.github.com>
2019-05-03 17:14:50 +0200

    Update load.py

--- load.py
+++ load.py
@@ -8,8 +8,7 @@ objets=[9,12,42,125,151,156,200,257,642,787,925,959]
 def load(objets):
     images={}
     for j in objets:
-        moon=[]
-        print(j)        
+        moon=[]      
         i=0
         filename='bases/'+str(j)+'/'+str(j)+'_r'
         moon.append(mpimg.imread(filename+str(int(i))+'.png'))
