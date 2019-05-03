f9811174ca8392180ead901a46d6ef354ae239b5
HubBrej <43384197+HubBrej@users.noreply.github.com>
2019-05-03 17:54:15 +0200

    Update LBP.py

--- LBP.py
+++ LBP.py
@@ -2,46 +2,65 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 import skimage.feature
+from load import *
+import random
+import cv2
 
+random.seed()
 
 objets=[9,12,42,125,151,156,200,257,642,787,925,959]
-
-
-radius = 1
+radius = 3
 n_points = 8 * radius
 
 
-hs={}
-def load(objets):
-    images={}
-    for j in objets:
-        moon=[]
-        print(j)        
-        i=0
-        filename='bases/'+str(j)+'/'+str(j)+'_r'
-        moon.append(mpimg.imread(filename+str(int(i))+'.png'))
-        for i in np.linspace(5,355,71):
-            moon.append(mpimg.imread(filename+str(int(i))+'.png'))
-        images[j]=moon
-    return images
+T={}
 
 images=load(objets)
+def computeLBPs(images):    
+    hs={}
+    for j in images.keys(): #parcourt les objets
+        print(j)
 
-for j in objets:
-    moon=images[j]
-    lbp=skimage.feature.local_binary_pattern(moon[0],n_points,radius,'ror')
-
-    h1=plt.hist(lbp.reshape(lbp.size),255)
-    h=h1[0]
-
-    for i in np.linspace(5,355,71):
-        lbp=skimage.feature.local_binary_pattern(moon[int(i/5)],n_points,radius,'ror')
+        moon=images[j]
+        lbp=skimage.feature.local_binary_pattern(moon[0],n_points,radius,'uniform') #premiere image
         
-        h1=plt.hist(lbp.reshape(lbp.size),255)
-        h=np.vstack((h,h1[0]))    
-    
-    hs[j]=np.mean(h,0)
+        h1=np.histogram(lbp.reshape(lbp.size),255) #creation histogramme
+        h1=np.take(h1[0],np.where(h1[0]>0))[0] 
+        
+        h=np.zeros((72,n_points+2),dtype=np.int64)
+        h[0]=h1
+        for i in range(len(moon)): #parcourt de toutes les images
+            
+            lbp=skimage.feature.local_binary_pattern(moon[i],n_points,radius,'uniform')
+            
+            h1=np.histogram(lbp.reshape(lbp.size),255)
+            h1=np.take(h1[0],np.where(h1[0]>0))[0]
+            
+            h[i]=h1
+        hs[j]=np.mean(h,0) #moyenne 
+    return hs
 
 plt.figure(0)
-plt.imshow(moon[int(i/5)],cmap='gray')
+
+hs=computeLBPs({k:images[k] for k in objets}) #compute all refs
+
+## image test aleatoire
+obj=objets[random.randint(0,12)]
+rotation=random.randint(0,73)
+htest=computeLBPs({0:[images[obj][rotation]]})
+
+
+## comparaison
+r=[]
+r2=[]
+plt.figure(0)
+for i in hs.keys():    
+    plt.plot(np.take(hs[i],np.where(hs[i]>0))[0],'.')
+    r.append(cv2.compareHist(np.float32(hs[i]),np.float32(htest[0]),0))
+    r2.append(cv2.compareHist(np.float32(hs[i]),np.float32(htest[0]),3))
+
+
+plt.figure(1)
+plt.imshow(images[obj][rotation],cmap='gray')
+
 plt.show()
